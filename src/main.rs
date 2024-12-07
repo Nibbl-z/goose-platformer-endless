5ddfc25598fb6ed6369fc57c71f0ebaf873803bc
Nibbl-z <thisisnotnibblz123@gmail.com>
2024-12-06 23:10:11 -0700

    GOOSE!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+fn conf() -> Conf {
+    Conf {
+        window_title: String::from("Goose Platformer Endless"),
+        fullscreen: false,
+        ..Default::default()
+    }
+}
+
+#[macroquad::main("Goose Platformer Endless")]
+async fn main() {
+    let goose = load_texture("img/player.png").await.unwrap();
+    
+    loop {
+        clear_background(WHITE);
+        
+        draw_texture(&goose, 100.0, 100.0, WHITE);
+
+        next_frame().await;
+    }
 }
